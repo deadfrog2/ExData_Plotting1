48c7c50378afc046bb580268303c1a7960700296
deadfrog2 <111714912+deadfrog2@users.noreply.github.com>
2022-11-15 16:42:14 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,18 @@
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+household_zip <- "household.zip"
+download.file(url, destfile = household_zip,method = "curl", mode="wb")
+unzip("household.zip",exdir = getwd())
+list.files()
+full_data <- read.table("household_power_consumption.txt",header = TRUE,
+                        sep = ";")
+head(full_data)
+dim(full_data)
+sub_data <- subset(full_data,Date %in% c("1/2/2007","2/2/2007"))
+head(sub_data)
+sub_data$Date <- as.Date(x=sub_data$Date,format="%d/%m/%Y")
+sub_data$Global_active_power <- as.numeric(sub_data$Global_active_power)
+sub_data$datetime <- as.POSIXct(paste(sub_data$Date,sub_data$Time))
+plot(sub_data$Global_active_power~sub_data$datetime,type="l",ylab = "Global Active
+     Power (kilowatts)",xlab="")
+dev.copy(png,"plot2.png",width = 480,height = 480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,30 @@
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+household_zip <- "household.zip"
+download.file(url, destfile = household_zip,method = "curl", mode="wb")
+unzip("household.zip",exdir = getwd())
+list.files()
+full_data <- read.table("household_power_consumption.txt",header = TRUE,
+                        sep = ";")
+head(full_data)
+dim(full_data)
+sub_data <- subset(full_data,Date %in% c("1/2/2007","2/2/2007"))
+head(sub_data)
+sub_data$Date <- as.Date(x=sub_data$Date,format="%d/%m/%Y")
+sub_data$Global_active_power <- as.numeric(sub_data$Global_active_power)
+sub_data$datetime <- as.POSIXct(paste(sub_data$Date,sub_data$Time))
+
+par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,2,0))
+plot(sub_data$Global_active_power ~ sub_data$datetime, type="l",
+     ylab="Global Active Power (kilowatts)",xlab="")
+plot(sub_data$Voltage~sub_data$datetime,type="l",
+     ylab="Voltage(volt)",xlab="")
+plot(sub_data$Sub_metering_1~sub_data$datetime,type="l",ylab=
+         "Global Active Power (Kilowatts)",xlab="")
+lines(sub_data$Sub_metering_2~sub_data$datetime,col="red")
+lines(sub_data$Sub_metering_3~sub_data$datetime,col="blue")
+legend("topright",col=c("black","red","blue"),cex=0.5,lty = 1,lwd = 2,legend = 
+           c("Sub_metering_1","Sub_metering_","Sub_metering_3"))
+plot(sub_data$Global_reactive_power~sub_data$datetime,type="l",
+     ylab = "Global Rective Power (kilowatts)",xlab="")
+dev.copy(png,"plot4.png",width=480,height=480)
+dev.off()
